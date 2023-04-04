a3829b304b1c64439cb818120606fcfd373a46a0
Asif Mehmuda <asif@Asifs-MacBook-Pro.local>
2023-04-04 15:59:18 -0600

    disable react dev tools added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import { TaskContextProviderComponent } from './contexts/TaskContext';
 import { AuthContextProviderComponent } from './contexts/authContext';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
